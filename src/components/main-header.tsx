aa15379e9e4f9bd1926e8ad81cb36b41c2a2d760
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-06-05 16:12:11 +0300

    Initial commit

---
+++ src/components/main-header.tsx
@@ -0,0 +1,12 @@
+const MainHeader = () => {
+    return (
+        <div className="w-full h-full text-center">
+            <h1 className="text-6xl text-slate-50 font-bold">Memorandum</h1>
+            <p className="text-xl text-slate-50">
+                Memo app for your daily notes
+            </p>
+        </div>
+    );
+};
+
+export default MainHeader;
